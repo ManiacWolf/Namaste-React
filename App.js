7b3e5a31c55a8b68eefd54b9cb0b5272fcea785b
Ankur Bansal <maniacwolf@Ankurs-MacBook-Pro.local>
2025-07-03 19:19:08 +0530

    L2-Ignition

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div", 
     {id: "parent"}, [
-        React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "H1 Parent Tag"),
-        React.createElement("h2", {}, "H2 Child Tag"),
+        React.createElement("div", {id: "child1", key: "child1"}, [
+        React.createElement("h1", { key: "h1"}, "H1 Parent Tag"),
+        React.createElement("h2", { key: "h2"}, "H2 Child Tag"),
         ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "H1 Parent Tag"),
-        React.createElement("h2", {}, "H2 Child Tag"),
+        // here I have used the key because of same elements and that was showing an error
+    React.createElement("div", {id: "child2", key: "child2" }, [
+        React.createElement("h1", { key: "h1"}, "H1 Parent Tag"),
+        React.createElement("h2", { key: "h2"}, "H2 Child Tag"),
         ]),
     ]);
 
